d6e84de8a8a474a9ca9f8f7ab6d9ef1a0ef333a6
pseudoical <pseudoical@gmail.com>
2026-09-22 18:00:01 -0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,33 @@
+# Apes Textures
+
+A collection of [Apes.io](https://apes.io/test) textures, along with a script for extracting images from the game's `.tex` files.
+
+# Download the textures
+
+## Clone the repository
+```bash
+git clone https://github.com/pseudoical/apes-textures.git
+```
+
+## [Download the ZIP](https://github.com//pseudoical/apes-textures/archive/refs/heads/main.zip)
+```text
+https://github.com//pseudoical/apes-textures/archive/refs/heads/main.zip
+```
+
+# Extract the textures
+
+```bash
+node ./src/textures.mjs
+```
+
+> Download the `.tex` files if needed and extract textures.
+
+```bash
+node ./src/textures.mjs --fetch
+```
+
+> Force download the `.tex` files and extract textures.
+
+---
+
+<p align="center">This project is licensed under the <a href="LICENSE">WTFPL, Version 2</a>.</p>
---
+++ src/textures.mjs
@@ -0,0 +1,65 @@
+// @ts-check
+
+/**
+ * Usage:
+ *   $ node ./src/textures.mjs
+ *       - Download the `.tex` files if needed and extract textures.
+ *   $ node ./src/textures.mjs --fetch
+ *       - Force download the `.tex` files and extract textures.
+ */
+
+import fs from "node:fs";
+import path from "node:path";
+
+/**
+ * @param {string} name
+ * @returns {Promise<void>}
+ */
+async function extractTextures(name) {
+    const texName = `${name}.tex`;
+    const texPath = path.join(import.meta.dirname, texName);
+
+    if (!fs.existsSync(texPath) || process.argv[2] === "--fetch") {
+        const response = await fetch(`https://apes.io/game/260916-28dd180-ls/${texName}`);
+        const start = performance.now();
+        const buffer = await response.arrayBuffer();
+        const seconds = (performance.now() - start) / 1000;
+        console.log(
+            `${(buffer.byteLength / 1024 / 1024 / seconds).toFixed(2)} MB/s`
+        );
+        await fs.promises.writeFile(texPath, Buffer.from(buffer));
+    }
+
+    const texturesPath = path.join("textures", name);
+    await fs.promises.mkdir(texturesPath, { recursive: true });
+
+    const tex = await fs.promises.readFile(texPath);
+
+    let cursor = 0;
+    const end = tex.length;
+
+    while (cursor !== end) {
+        const meta = tex.subarray(cursor, cursor + 6);
+        cursor += 6;
+
+        const fileNameLength = meta[1];
+        const imageDataLength = meta[2] | meta[3] << 8 | meta[4] << 16 | meta[5] << 24;
+
+        const fileName = tex.toString("utf8", cursor, cursor + fileNameLength);
+        cursor += fileNameLength;
+
+        const imageData = tex.subarray(cursor, cursor + imageDataLength);
+        cursor += imageDataLength;
+
+        const type = 1 & meta[0] ? "jpeg" : "png";
+        const imagePath = path.join(texturesPath, `${fileName}.${type}`);
+
+        // Write files concurrently for performance.
+        fs.promises.writeFile(imagePath, imageData);
+    }
+}
+
+for (const name of ["init", "game"]) {
+    // Extract textures concurrently for performance.
+    extractTextures(name);
+}
